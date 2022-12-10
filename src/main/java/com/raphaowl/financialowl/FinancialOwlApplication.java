2013632afd818800d31de67a20901c534e385210
Raphael Torres <raphael.torres@trustly.com>
2022-12-10 11:58:06 -0300

    first-commit

---
+++ src/main/java/com/raphaowl/financialowl/FinancialOwlApplication.java
@@ -0,0 +1,13 @@
+package com.raphaowl.financialowl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialOwlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialOwlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raphaowl/financialowl/FinancialOwlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raphaowl.financialowl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialOwlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
